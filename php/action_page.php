ec910d57dd0ee41517d88482441036ab5ddc9848
saurabh9495 <saurabhkumar4829@gmail.com>
2017-07-06 14:36:51 +0530

    Add files via upload

---
+++ php/action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<p>Your response has been received . Thanks for giving feedback. 
+
+</body>
+</html> 

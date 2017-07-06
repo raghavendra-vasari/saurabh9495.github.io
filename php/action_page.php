3fccab07f0b06bd1c7e495cb8f19e1b6a76a5a6c
saurabh9495 <saurabhkumar4829@gmail.com>
2017-07-06 23:33:28 +0530

    Update action_page.php

--- php/action_page.php
+++ php/action_page.php
@@ -1,9 +1,19 @@
 <html>
 <body>
-
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-<p>Your response has been received . Thanks for giving feedback. 
-
+<?php
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['email'])) {
+    $data = $_POST['name'] . '-' . $_POST['email'] . '-' . $_POST['email'] . "\n";
+    $ret = file_put_contents('/tmp/response.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file and your response has been received ,thanks for giving your feeback";
+    }
+}
+else {
+   die('no post data to process');
+}
+?>
 </body>
 </html> 
